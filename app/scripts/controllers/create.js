7a9f34ea9067381849c6a06aa2954e7c3837898e
Steven Bazyl <sqrrrl@gmail.com>
2013-04-01 15:57:01 -0700

    Add license headers

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -1,3 +1,17 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 'use strict';
 
 angular.module('todos').controller('CreateCtrl', ['$scope', '$location', 'storage',
@@ -9,7 +23,7 @@ angular.module('todos').controller('CreateCtrl', ['$scope', '$location', 'storag
    * @param {!object} storage
    * @constructor
    */
-    function ($scope, $location, storage) {
+  function ($scope, $location, storage) {
     $scope.message = 'Please wait';
     storage.requireAuth().then(function () {
       storage.createDocument('New Project').then(function (file) {
